3f9c92710b0728f4e2961b1f3e628153822ad23b
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-12-07 23:46:57 +0900

    fix: time over 였던 첫번째 방법 수정 (#2)

--- higherOrderFunction/primeFactorization.swift
+++ higherOrderFunction/primeFactorization.swift
@@ -1,9 +1,11 @@
 import Foundation
 
 func solution(_ n:Int) -> [Int] {
-  
     // max : 17ms memory : 16.4MB
     return (2...n).filter { n % $0 == 0 && isPrime($0) }
+  
+    // max : 20ms
+    //return (2...n).filter { n % $0 == 0 }.filter { isPrime($0) } 
         
     // time over 
     //return (2...n).filter { isPrime($0) }.filter { n % $0 == 0 } 
