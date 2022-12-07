4c5725ba69c9ac4668a319c39e62ac960fc2e512
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-12-07 23:37:40 +0900

    feat: 소인수분해 문제 코드 추가

---
+++ higherOrderFunction/primeFactorization.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ n:Int) -> [Int] {
+  
+    // max : 17ms memory : 16.4MB
+    return (2...n).filter { n % $0 == 0 && isPrime($0) }
+        
+    // time over 
+    //return (2...n).filter { isPrime($0) }.filter { n % $0 == 0 } 
+}
+ 
+func isPrime(_ n:Int) -> Bool {
+    return (2...n).filter { n % $0 == 0}.count == 1 ? true : false
+}
