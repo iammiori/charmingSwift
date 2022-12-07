38eb5f7bb0aaee19dd996501902aa98e43aec2f9
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-12-07 23:49:39 +0900

    feat: 소인수분해 for-in 과 if 를 사용해서 푸는 방법 추가  (#3)

--- higherOrderFunction/primeFactorization.swift
+++ higherOrderFunction/primeFactorization.swift
@@ -1,7 +1,16 @@
 import Foundation
 
 func solution(_ n:Int) -> [Int] {
+    // max : 11ms 평균적으로는 filter보다 빠름
+    // var result: [Int] = []
+    // for num in (2...n) {
+    //     if (n % num == 0 && isPrime(num)) {
+    //         result.append(num)
+    //     }
+    // }
+    // return result
     // max : 17ms memory : 16.4MB
+    
     return (2...n).filter { n % $0 == 0 && isPrime($0) }
   
     // max : 20ms
